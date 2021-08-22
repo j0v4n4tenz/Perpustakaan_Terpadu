d1921bc64c5daec39060fc18114b681cc03186df
Levijovanatan <76637974+Levijovanatan@users.noreply.github.com>
2021-08-22 23:51:56 +0700

    folder

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Komputer", "Memasak", "Matematika", "Designer", "Olahraga","Fashion","Otomotif"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
